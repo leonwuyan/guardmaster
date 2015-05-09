5709cd6e97e03e32c0fb4cb6f47d2d2ada885f85
Raynor Zhong <raynor41@gmail.com>
2015-05-09 16:46:16 +0800

    add _ to urls

--- detection/urls.py
+++ detection/urls.py
@@ -3,23 +3,23 @@ from . import views, auth_views
 
 urlpatterns = [
     url(
-        r'^(?P<panel_id>[0-9]+)/user_query/(?P<url>[A-Za-z]+)$',
+        r'^(?P<panel_id>[0-9]+)/user_query/(?P<url>[_A-Za-z]+)$',
         views.user_query,
         name='user_query'),
     url(
-        r'^(?P<panel_id>[0-9]+)/gang_query/(?P<url>[A-Za-z]+)$',
+        r'^(?P<panel_id>[0-9]+)/gang_query/(?P<url>[_A-Za-z]+)$',
         views.gang_query,
         name='gang_query'),
     url(
-        r'^(?P<panel_id>[0-9]+)/deal_query/(?P<url>[A-Za-z]+)$',
+        r'^(?P<panel_id>[0-9]+)/deal_query/(?P<url>[_A-Za-z]+)$',
         views.deal_query,
         name='deal_query'),
     url(
-        r'^(?P<panel_id>[0-9]+)/(?P<t_p>[_A-Za-z]+)/(?P<url>[A-Za-z]+).json$',
+        r'^(?P<panel_id>[0-9]+)/(?P<t_p>[_A-Za-z]+)/(?P<url>[_A-Za-z]+).json$',
         views.json_template,
         name='json_template'),
     url(
-        r'^(?P<panel_id>[0-9]+)/count/(?P<url>[A-Za-z]+)$',
+        r'^(?P<panel_id>[0-9]+)/count/(?P<url>[_A-Za-z]+)$',
         views.count,
         name='count'),
     url(r'^$', auth_views.index, name='default_index'),
--- operating/urls.py
+++ operating/urls.py
@@ -4,15 +4,15 @@ from detection import auth_views, views
 
 urlpatterns = [
     url(
-        r'^(?P<panel_id>[0-9]+)/(?P<t_p>[_A-Za-z]+)/(?P<url>[A-Za-z]+).json$',
+        r'^(?P<panel_id>[0-9]+)/(?P<t_p>[_A-Za-z]+)/(?P<url>[_A-Za-z]+).json$',
         views.json_template,
         name='json_template'),
     url(
-        r'^(?P<panel_id>[0-9]+)/notify/(?P<url>[A-Za-z]+)$',
+        r'^(?P<panel_id>[0-9]+)/notify/(?P<url>[_A-Za-z]+)$',
         operating_views.notify,
         name='notify'),
     url(
-        r'^(?P<panel_id>[0-9]+)/mail/(?P<url>[A-Za-z]+)$',
+        r'^(?P<panel_id>[0-9]+)/mail/(?P<url>[_A-Za-z]+)$',
         operating_views.mail,
         name='mail'),
     url(r'^$', auth_views.index, name='default_index'),
