2d36af5057acb7acecf12cf1d8e9acc007209227
Raynor Zhong <raynor41@gmail.com>
2015-07-02 18:57:16 +0800

    fix bug in rsync

---
+++ deployment/models.py
@@ -0,0 +1,6 @@
+from django.utils.translation import ugettext_lazy as _
+from detection.models import Panel
+from django.db import models
+
+# Create your models here.
+
--- guardmaster/settings.py
+++ guardmaster/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'detection',
     'operating',
+    'deployment',
 )
 
 MIDDLEWARE_CLASSES = (
